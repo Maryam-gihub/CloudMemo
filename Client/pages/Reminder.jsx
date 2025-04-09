1a2ba2eada16b52868bc13491873bd1562fe5abe
Maryam <ayomidemaryam310@gmail.com>
2025-04-09 12:24:37 +0100

    pull madam,stop pressing phone

--- Client/pages/Reminder.jsx
+++ Client/pages/Reminder.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import SIdeNavbar from '../components/SIdeNavbar'
 
 const Reminder = () => {
     return (
-        <div>Reminder</div>
+        <div>
+            <Navbar />
+            <div className='d-flex mt-4 gap-5'>
+                <SIdeNavbar />
+                <div className='border border-dark border-3 col-9 p-4'>
+                    <h1>My Notes</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
