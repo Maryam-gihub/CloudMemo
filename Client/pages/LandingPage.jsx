8e69e222c647daac416df34533cc7f27ef6b15fa
Caroline <ajiboyecaroline95@gmail.com>
2025-04-13 14:09:15 +0100

    folder displaying

--- Client/pages/LandingPage.jsx
+++ Client/pages/LandingPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import SIdeNavbar from '../components/SIdeNavbar'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 const LandingPage = () => {
-const add =()=>{
-    note.style.display = 'none'
-    note2.style.display = 'none'
+    const [Folder, setFolder] = useState('')
+    const [Container, setContainer] = useState([])
+    const add = () => {
+        if (Folder === '') {
+            alert('Enter your folder name')
+        } else {
+            lol.style.display = 'none'
+            msg.style.display = 'none'
+            const push = [...Container, Folder]
+            setContainer(push)
+            console.log(push);
+        }
 
-}
+    }
     return (
         <div className='w-100'>
             <Navbar />
@@ -15,19 +27,28 @@ const add =()=>{
                 <SIdeNavbar />
                 <div className='col-9 p-4'>
                     <div className='d-flex mx-auto justify-content-center gap-2'>
-                        <input type="text" className='col-lg-6 py-lg-2 px-lg-4 px-3 rounded-3 col-10 border border-dark shadow'/>
-                        <button className = 'btn shadow' onClick = {add} id = 'plus' style = {{backgroundColor: 'black'}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-plus-lg text-light fw-bold" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
-                        </svg>
+                        <input type="text" onChange={(e) => setFolder(e.target.value)} value={Folder} className='col-lg-6 py-lg-2 px-lg-4 px-3 rounded-3 col-10 border border-dark shadow' />
+                        <button className='btn shadow' onClick={add} id='plus' style={{ backgroundColor: 'black' }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-plus-lg text-light fw-bold" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+                            </svg>
                         </button>
                     </div>
-                    <div className='d-flex justify-content-center align-items-center' style={{ height: '60vh' }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" id = 'note' style = {{display : 'block'}} fill="currentColor" className="bi bi-plus-lg text-secondary text-opacity-75" viewBox="0 0 16 16">
+                    {Container.map((Folder, index) => (
+                        <Link to={'/note'}>
+                        <div className='mt-5 gap-2 d-flex flex-row flex-wrap'>
+                            <button className='col-lg-2 col-5 p-lg-4 p-2 border border-dark border-3 text-break' style={{ backgroundColor: 'red' }}>
+                                <p className='bg-light fw-bold px-3 text-dark text-capitalize'>{Folder}</p>
+                            </button>
+                        </div>
+                        </Link>
+                    ))}
+                    <div className='d-flex justify-content-center align-items-center' style={{ height: '60vh', display: 'block' }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"  id = 'lol' fill="currentColor" className="bi bi-plus-lg text-secondary text-opacity-75" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
                         </svg>
                     </div>
-                    <p className='text-center fw-bold fs-4 text-secondary text-opacity-50' style = {{display : 'block'}} id = 'note2'>Create a folder.......</p>
+                    <p className='text-center fw-bold fs-4 text-secondary text-opacity-50' id = 'msg' style = {{display: 'block'}}>Create a folder.......</p>
                 </div>
             </div>
         </div>
--- Client/pages/Note.jsx
+++ Client/pages/Note.jsx
@@ -10,9 +10,9 @@ const Note = () => {
                 <div className='d-flex mt-2 gap-2'>
                     <SIdeNavbar />
                     <div className='col-9 p-4'>
-                        <textarea name="" id="" cols="30" className = 'w-100 rounded-5' style = {{height : '80vh'}}></textarea>
-                        <div className = 'd-flex justify-content-end'>
-                            <button className = 'btn px-lg-5 px-3 fs-6 py-lg-2 py-1 fw-bold' style = {{backgroundColor: 'rgb(23, 253, 199)'}}>
+                        <textarea name="" id="" cols="30" className = 'w-100 rounded-5 p-4 overflow-y-auto form-control border border-dark border-3 fs-6' style = {{height : '80vh'}}></textarea>
+                        <div className='d-flex justify-content-end'>
+                            <button className='btn px-lg-5 px-3 fs-6 py-lg-2 py-1 fw-bold' style={{ backgroundColor: 'rgb(23, 253, 199)' }}>
                                 Save
                             </button>
                         </div>
