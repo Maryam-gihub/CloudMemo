4d044cf81dffb4b67ab950a9d9b9dcb9f39516db
Caroline <ajiboyecaroline95@gmail.com>
2025-05-12 20:09:51 +0100

    note is working

--- Client/pages/Note.jsx
+++ Client/pages/Note.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import SIdeNavbar from '../components/SIdeNavbar'
 
-
-const Note = () => {
+const note = () => {
     return (
         <div>
             <div className='w-100'>
-                <Navbar />
+                <Navbar/>
                 <div className='d-flex  gap-2'>
-                    <SIdeNavbar />
+                    <SIdeNavbar/>
                     <div className='col-8 mx-auto p-4 mt-3 ps-5'>
-                        <textarea name="" id="" cols="30" className = 'w-100 rounded-5 p-4 overflow-y-auto form-control border border-dark border-3 fs-6' style = {{height : '80vh'}}></textarea>
+                        <textarea name="" id="" cols="30" className='w-100 rounded-5 p-4 overflow-y-auto form-control border border-dark border-3 fs-6' style={{ height: '80vh' }}></textarea>
                         <div className='d-flex justify-content-end mt-2'>
                             <button className='btn px-lg-5 px-3 fs-6 py-lg-2 py-1 fw-bold' style={{ backgroundColor: 'rgb(23, 253, 199)' }}>
                                 Save
@@ -17,9 +18,9 @@ const Note = () => {
                         </div>
                     </div>
                 </div>
-                </div>
+            </div>
         </div>
     )
 }
 
-export default Note
+export default note
