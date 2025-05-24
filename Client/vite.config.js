27694e0448db95fc0b533d3e0af42f3f90db4d56
Maryam <ayomidemaryam310@gmail.com>
2025-05-24 16:09:37 +0100

    error

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-VitePWA
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), VitePWA()],
   build: {
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name]. [hash].js',
-        entryFileNames: 'assets/[name]. [hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return 'vendor';
@@ -22,8 +20,8 @@ export default defineConfig({
     },
   },
   publicDir: 'public',
-  // Add the following lines to specify the location of your manifest file 
   manifest: {
     srcPath: './public/manifest.json',
   },
 })
+
