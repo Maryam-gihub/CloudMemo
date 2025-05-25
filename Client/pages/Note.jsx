eea02e01b6bc3bbfb934173379b9b527309d994b
Maryam <ayomidemaryam310@gmail.com>
2025-05-25 22:13:44 +0100

    fix: correct import path for React in Navbar and LandingPage components style: update textarea border class in Note component

--- Client/pages/Note.jsx
+++ Client/pages/Note.jsx
@@ -10,7 +10,7 @@ const note = () => {
                 <div className='d-flex  gap-2'>
                     <SideNavbar/>
                     <div className='col-lg-7 mx-auto col-8 pt-4'>
-                        <textarea name="" id="" cols="30" className='w-100 rounded-5 p-4 overflow-y-auto form-control border border-dark border-3 fs-6' style={{ height: '80vh' }}></textarea>
+                        <textarea name="" id="" cols="30" className='w-100 rounded-5 p-4 overflow-y-auto form-control border-dark border-3 fs-6' style={{ height: '80vh' }}></textarea>
                         <div className='d-flex justify-content-between mt-2'>
                             <button className='btn px-lg-5 px-3 fs-6 py-lg-2 py-1 fw-bold' style={{ backgroundColor: 'rgb(23, 253, 199)' }}>Back</button>
                             <button className='btn px-lg-5 px-3 fs-6 py-lg-2 py-1 fw-bold' style={{ backgroundColor: 'rgb(23, 253, 199)' }}>Save</button>
