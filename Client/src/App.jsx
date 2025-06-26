43f4084c60d315722a748ed5dafdd6aca34739c6
Caroline <ajiboyecaroline95@gmail.com>
2025-06-26 12:55:31 +0100

    working

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/LandingPage'
-import Reminder from './pages/Reminder'
 import Settings from './pages/Settings'
 import Trash from './pages/Trash'
 import Archived from './pages/Archived'
 import Note from './pages/Note'
-
+import Reminder from './pages/Reminder'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,13 +26,13 @@ function App() {
 
   return (
     <Routes>
-        <Route index element={<LandingPage />}/>
+        <Route path= '/' element={<LandingPage />}/>
         <Route path='/remind' element={<Reminder/>}/>
         <Route path="/trash" element={<Trash />} />
         <Route path="/set" element={<Settings />} />
         <Route path="/archive" element={<Archived />} />
         <Route path="/note" element={<Note />} />
-    </Routes>
+        </Routes>
   )
 }
 
